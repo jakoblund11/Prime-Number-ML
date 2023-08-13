f3bcf92f3379650aab35ff36cf6b78e9df739efe
jakoblund11 <98823878+jakoblund11@users.noreply.github.com>
2023-08-13 20:49:48 +0200

    Updated algorithm and scoring

--- prime_ml.py
+++ prime_ml.py
@@ -1,67 +1,53 @@
-from tqdm import tqdm
+from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import *
 from sklearn.ensemble import *
 from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVR
+from sklearn.svm import NuSVC
 from sklearn.kernel_ridge import KernelRidge
 from sklearn.neural_network import MLPClassifier
+from sklearn.mixture import BayesianGaussianMixture
 import numpy as np
+import pandas as pd
 
-def prime_range(start, end):
-    assert start > 1, "start must be larger than 1"
-    result = []
-    for tal in range(start, end + 1):
-        prime = True
-        for tal2 in range(2, int(tal ** 0.5) + 1):
-            if (tal % tal2) == 0:
-                prime = False
-                break
+# Read the data from the CSV file into a DataFrame
+data = pd.read_csv('data.csv', nrows=1000)
 
-        if prime:
-            result.append(tal)
-    return result
+# Extract the features and target variable from the DataFrame
+#X = data.drop(['Modulo 2', 'Modulo 3', 'Modulo 5', 'Modulo 7', 'Modulo 11', 'Modulo 13', 'Sum of digits', 'Prime'], axis=1)
+X = data.drop(['Prime'], axis=1)
+y = data['Prime']
 
-
-first_num = 2
-last_num = 1000000
-
-number_line = np.array([x for x in range(first_num, last_num + 1)])
-
-prime_number_line = np.array(prime_range(first_num, last_num + 1))
-
-# following is true/false list of whether a number is prime or not starting from 2 to end
-if not last_num == 1000000:
-    true_false = [item in prime_number_line for item in tqdm(number_line)]
-
-with open('Primes_true_false.txt', 'r') as f:
-    if last_num == 1000000:
-        true_false = f.read()
-        lst = true_false[1:-1].split(', ')
-        lst = [True if x == 'True' else False for x in lst]
-        true_false = lst
-    pass
-
-
-train_size = 0.5
-X_train, X_test, y_train, y_test = train_test_split(number_line.reshape(-1, 1), true_false, train_size=train_size,
+train_size = 0.2
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=train_size,
                                                     test_size=1 - train_size, shuffle=False)
 
 #clf = LogisticRegression().fit(X_train, y_train) # Accuracy of 0.921845
 #clf = LinearRegression().fit(X_train, y_train) # Accuracy of 0.0005095
 #clf = KernelRidge().fit(X_train, y_train) # Requires 4.66 TiB RAM
-clf = MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=(10, 5, 2)).fit(X_train, y_train)
+#clf = BayesianGaussianMixture().fit(X_train, y_train)
+clf = LinearSVC(max_iter=1000000000).fit(X_train, y_train) # Does not predict everything to be non-prime! That is a great start
+#clf = LinearSVR(max_iter=1000000000).fit(X_train, y_train)
+#clf = MLPClassifier(solver='adam', activation='relu', alpha=1e-5, hidden_layer_sizes=(5, 2)).fit(X_train, y_train)
 
 y_pred = clf.predict(X_test)
 
 print("Accuracy:", clf.score(X_test, y_test))
 
+
 # Find the indices of the data that the model got wrong
-wrong_indices = [i for i in range(len(y_test)) if y_test[i] != y_pred[i]]
+wrong_indices = [i for i in range(len(y_test)) if y_test.iloc[i] != y_pred[i]]
 
 # Print the true labels and predicted labels for the data that the model got wrong
-print("Wrong numbers:", [X_test[i][0] for i in wrong_indices])
+#print("Wrong numbers:", [X_test.iloc[i][0] for i in wrong_indices])
 #print("True labels:", [y_test[i] for i in wrong_indices])
 print("Number of numbers being falsely labeled as false:", sum([y_pred[i] for i in wrong_indices]))
 print("This means the model always predicts a number as a non-prime")
 print("Number of wrong predictions:", len(wrong_indices))
-print("Number of primes:", len(prime_number_line))
+print("Number of primes:", sum(data.Prime))
+print("Wrong predictions divided by total number of primes:", len(wrong_indices)/sum(data.Prime))
+print('AUC-ROC:', roc_auc_score(y_test, y_pred))
+
+#nrows=1000 gives Accuracy: 0.86625, Number of numbers being falsely labeled as false: 9, and AUC-ROC: 0.591723487596112
